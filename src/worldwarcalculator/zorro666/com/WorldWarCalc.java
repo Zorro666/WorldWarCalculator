6f95a75477f36dfe1e3d22102b33a2f7139dcd60
Jake <jake@evansturner.co.uk>
2010-05-02 22:07:04 +0100

    trap number input

--- src/worldwarcalculator/zorro666/com/WorldWarCalc.java
+++ src/worldwarcalculator/zorro666/com/WorldWarCalc.java
@@ -3,11 +3,18 @@ package worldwarcalculator.zorro666.com;
 import android.app.Activity;
 import android.widget.TabHost;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.util.Log;
 
-public class WorldWarCalc extends Activity {
+public class WorldWarCalc extends Activity implements TextWatcher
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
@@ -26,5 +33,32 @@ public class WorldWarCalc extends Activity {
         spec.setIndicator("Defence");
         tabs.addTab(spec);
         tabs.setCurrentTab(0);
+        
+        m_value1 = (TextView)findViewById(R.id.IncomeBuilding1Value);
+        EditText value = (EditText)findViewById(R.id.IncomeBuilding1Number);
+        value.addTextChangedListener(this);
+        
     }
+    
+    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+    {
+    }
+    
+    public void onTextChanged(CharSequence s, int start, int count, int after)
+    {
+    }
+    
+    public void afterTextChanged(Editable e)
+    {
+    	if (e.length() > 0)
+    	{
+    		int number = Integer.parseInt(e.toString());
+    		Log.i(TAG, "value="+Integer.toString(number));
+    	}
+    	
+    }
+    
+    private TextView m_value1;
+    private static final String TAG = "WWCALC";
+    
 }
